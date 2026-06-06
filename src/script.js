e6ad1073846cccf3f5de7af9a33d3e45d766d2ce
Nimira43 <n.rabone72@aol.com>
2026-06-06 23:23:32 +0100

    Project setup

---
+++ src/script.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+const gui = new GUI({ width: 340 })
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterMaterial = new THREE.MeshBasicMaterial()
+const water = new THREE.Mesh(waterGeometry, waterMaterial)
+water.rotation.x = - Math.PI * 0.5
+scene.add(water)
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.set(1, 1, 1)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(
+  Math.min(window.devicePixelRatio, 2)
+)
+
+const clock = new THREE.Clock()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+
+tick()
