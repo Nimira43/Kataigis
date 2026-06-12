3b8652caae1afdbc869e5eee6c0837745cc7c5ac
Nimira43 <n.rabone72@aol.com>
2026-06-12 02:26:49 +0100

    Basic animation and GUI controls

--- src/script.js
+++ src/script.js
@@ -14,11 +14,45 @@ const waterMaterial = new THREE.ShaderMaterial({
   vertexShader: waterVertexShader,
   fragmentShader: waterFragmentShader,
   uniforms: {
+    uTime: {
+      value: 0
+    },
     uBigWavesElevation: {
       value: 0.2
+    },
+    uBigWavesFrequency: {
+      value: new THREE.Vector2(4, 1.5)
+    },
+    uBigWavesSpeed: {
+      value: 0.75
     }
   }
 })
+
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name('uBigWavesElevation')
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('uBigWavesFrequency X')
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('uBigWavesFrequency Y')
+gui
+  .add(waterMaterial.uniforms.uBigWavesSpeed, 'value')
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name('uBigWavesSpeed')
 
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
 water.rotation.x = - Math.PI * 0.5
@@ -61,6 +95,7 @@ renderer.setPixelRatio(
 const clock = new THREE.Clock()
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+  waterMaterial.uniforms.uTime.value = elapsedTime
   controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
