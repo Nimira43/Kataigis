cd63acb01ffec23b00a7b9476d474a5d32ee0cb5
Nimira43 <n.rabone72@aol.com>
2026-06-22 22:25:48 +0100

    Added Perlin 3D noise. Project completed.

--- src/script.js
+++ src/script.js
@@ -10,10 +10,24 @@ const debugObject = {}
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
 
-debugObject.depthColour = '#0000ff'
-debugObject.surfaceColour = '#8888ff'
+debugObject.depthColour = '#186691'
+debugObject.surfaceColour = '#9bd8ff'
+
+gui
+  .addColor(debugObject, 'depthColour')
+  .onChange(() => {
+    waterMaterial.uniforms.uDepthColour.value
+      .set(debugObject.depthColour)
+  })
+
+gui
+  .addColor(debugObject, 'surfaceColour')
+  .onChange(() => {
+    waterMaterial.uniforms.uSurfaceColour.value
+      .set(debugObject.surfaceColour)
+  })
 
 const waterMaterial = new THREE.ShaderMaterial({
   vertexShader: waterVertexShader,
@@ -31,6 +45,18 @@ const waterMaterial = new THREE.ShaderMaterial({
     uBigWavesSpeed: {
       value: 0.75
     },
+    uSmallWavesElevation: {
+      value: 0.15
+    },
+    uSmallWavesFrequency: {
+      value: 3
+    },
+    uSmallWavesSpeed: {
+      value: 0.2
+    },
+    uSmallIterations: {
+      value: 4
+    },
     uDepthColour: {
       value: new THREE.Color(debugObject.depthColour)
     },
@@ -38,10 +64,10 @@ const waterMaterial = new THREE.ShaderMaterial({
       value: new THREE.Color(debugObject.surfaceColour)
     },
     uColourOffset: { 
-      value: 0.25
+      value: 0.08
     },
     uColourMultiplier: {
-      value: 2
+      value: 5
     },
   }
 })
@@ -71,17 +97,29 @@ gui
   .step(0.001)
   .name('uBigWavesSpeed')
 gui
-  .addColor(debugObject, 'depthColour')
-  .name('depthColour')
-  .onChange(() => {
-    waterMaterial.uniforms.uDepthColour.value.set(debugObject.depthColour)
-  })
+  .add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name('uSmallWavesElevation')
 gui
-  .addColor(debugObject, 'surfaceColour')
-  .name('surfaceColour')
-  .onChange(() => {
-    waterMaterial.uniforms.uSurfaceColour.value.set(debugObject.surfaceColour)
-  })
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name('uSmallWavesFrequency')
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name('uSmallWavesSpeed')
+gui
+  .add(waterMaterial.uniforms.uSmallIterations, 'value')
+  .min(0)
+  .max(5)
+  .step(1)
+  .name('uSmallIterations')
 gui
   .add(waterMaterial.uniforms.uColourOffset, 'value')
   .min(0)
@@ -94,8 +132,6 @@ gui
   .max(10)
   .step(0.001)
   .name('uColourMultiplier')
-
-
 
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
 water.rotation.x = - Math.PI * 0.5
